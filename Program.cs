5a64c1cf6276db40266526f8a21944632d100aab
grogsden <brian.stempien@protonmail.com>
2021-05-04 18:36:29 -0500

    started conversation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace firstC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, how are you?");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, nice to meet you. My name is Console. I'm going to kill you.");
         }
     }
 }
